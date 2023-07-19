2fd0d3eb2cbfb377018228f39c95b6851110b474
AS-Coope <deltazirconium@gmail.com>
2023-07-18 21:03:52 -0500

    Add structure to the project

--- OptionsMenu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready():
-	$VBoxContainer/MainMenu.grab_focus()
-
-# Switches to the Main Menu
-func _on_MainMenu_pressed():
-	get_tree().change_scene("res://MainMenu.tscn")
